1bf8fccae25981e43e92d927af06f74e6804d84c
Wagner Goulart <wcfx.dev@gmail.com>
2021-11-09 18:22:33 -0300

    chore: config fonts in xcode

--- src/modules/Home/index.tsx
+++ src/modules/Home/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { TouchableOpacity } from 'react-native';
 
 // import {  } from '~/components';
 // import {  } from '~/assets';
@@ -7,9 +8,13 @@ import React from 'react';
 import * as S from './styles';
 
 const Home = () => {
+  const handleFuction = () => {
+    console.log('handleFuction');
+  };
   return (
     <S.Container>
       <S.Title>Hello Motherfuckers</S.Title>
+      <TouchableOpacity onPress={handleFuction} />
       <S.Description>Wagnifico na parada</S.Description>
     </S.Container>
   );
